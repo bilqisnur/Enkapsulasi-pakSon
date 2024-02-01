263f16f5d1971f192c6cd02473f06b189a7c20e0
bilqisnur <bilqis_nur_32rpl@student.smktelkom-mlg.sch.id>
2024-02-01 08:47:02 +0700

    first commit

---
+++ main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class main {
+    public static void main(String[] args) {
+     person mPerson = new person();
+     mPerson.setName("bilqis");
+        System.out.println("Person name : "+mPerson.getName());
+    }
+         
+    }
+    
+
---
+++ person.java
@@ -0,0 +1,43 @@
+
+public class person {
+    private String name;
+    private String username;
+    private String password;
+    
+    public person(){
+        System.out.println("eksekusi method constructor....");
+    }
+    
+    public person(String a){
+        System.out.println("eksekusi method constructor....");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+    
+    
+    
+}
